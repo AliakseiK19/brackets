b2a707b06f96987d077a896c7a3f3eed54f3cb49
AliakseiK19 <positive_on@yahoo.com>
2019-03-23 16:25:56 +0300

    -)1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-      
-if (str.match(bracketsConfig))
-{
-    return true;
-}
-else {return false;}
-}
+    let tempStr = str;
+    for (let strKey = tempStr.length-1; strKey>=0; strKey--)
+    {
+        for (let bracketsKey = 0; bracketsKey<bracketsConfig.length; bracketsKey++)
+        {
+          if(tempStr[strKey]===bracketsConfig[bracketsKey][0])
+          {
+             tempStr = tempStr.replace(bracketsConfig[bracketsKey].join(''), '');
+          }
+        }
+    }
+    return tempStr ==='';
+  }
